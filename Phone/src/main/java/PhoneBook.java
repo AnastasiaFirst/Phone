c3c4055a9bb65d41dc6a3e62534b698d8e60b0e2
Anastasia Bobyr <BobyrAS@it.mos.ru>
2024-12-08 19:11:24 +0300

    add implemented

--- Phone/src/main/java/PhoneBook.java
+++ Phone/src/main/java/PhoneBook.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> contacts;
+
+    public PhoneBook() {
+        this.contacts = new HashMap<>();
+    }
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
