aea2638e18cc5c41d372d65329ad5678d597f76f
Anastasia Bobyr <BobyrAS@it.mos.ru>
2024-12-08 19:08:02 +0300

    Stub and tests for add

--- Phone/src/test/java/PhoneBookTest.java
+++ Phone/src/test/java/PhoneBookTest.java
@@ -1,5 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PhoneBookTest {
+    private PhoneBook phoneBook;
 
+    @BeforeEach
+    void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    void testAddContact() {
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(2, phoneBook.add("Bob", "67890"));
+        assertEquals(2, phoneBook.add("Alice", "54321")); // Дубликат не добавляется
+    }
 }
